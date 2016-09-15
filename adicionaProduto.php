af793739bf77373ffbfcb4899b32071ed7996384
Renoir Faria <renofaria@gmail.com>
2016-09-15 20:17:37 -0300

    2. Inserindo produtos no banco de dados

--- adicionaProduto.php
+++ adicionaProduto.php
@@ -1,12 +1,24 @@
 <?php
+include 'cabecalho.php';
 $nome = $_GET['nome'];
 $preco = $_GET['preco'];
-include 'cabecalho.php';
-?>
-<p class="alert alert-success">
-  Produto <?php echo $nome; ?>, <?=$preco?> adicionado com sucesso!
-</p>
-<?php
+$conexao = mysqli_connect('localhost','root','','loja');
+
+$query = "insert into produtos (nome,preco) values ('{$nome}',{$preco})";
+if(mysqli_query($conexao,$query)){
+  ?>
+  <p class="alert alert-success">
+    Produto <?php echo $nome; ?>, <?=$preco?> adicionado com sucesso!
+  </p>
+  <?php
+} else {
+  ?>
+  <p class="alert alert-danger">
+    O produto <?=$nome?> não foi adicionado!
+  </p>
+  <?php
+}
+
 $nome = $_GET['nome'];
 $preco = $_GET['preco'];
 include 'rodape.php';
