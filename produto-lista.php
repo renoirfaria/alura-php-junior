640ffc307334b357396d5b8457ae7d32f22abe3f
Renoir Faria <renofaria@gmail.com>
2016-09-15 21:35:28 -0300

    6. Deletando produtos

--- produto-lista.php
+++ produto-lista.php
@@ -4,7 +4,11 @@ include 'conecta.php';
 include 'banco-produto.php';
 ?>
 
-
+<?php if(array_key_exists('removido',$_GET) && $_GET['removido'] == 'true'): ?>
+<p class="alert alert-success">
+  Produto apagado com sucesso
+</p>
+<?php endif ?>
 
 
 <table class="table table-striped table-bordered">
@@ -18,6 +22,9 @@ include 'banco-produto.php';
     </td>
     <td>
       <?=$produto['preco']?>
+    </td>
+    <td>
+      <a href="remove-produto.php?id=<?=$produto['id']?>" class="text-danger">Remover</a>
     </td>
   </tr>
 <?php endforeach ?>
