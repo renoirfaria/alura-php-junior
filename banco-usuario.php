fa011fd8860558bddf4eba5b3c2b4218d8aca38c
Renoir Faria <renofaria@gmail.com>
2016-09-20 13:23:13 -0300

    8. SQL Injection

--- banco-usuario.php
+++ banco-usuario.php
@@ -2,6 +2,7 @@
 
 function buscaUsuario($conexao,$email,$senha){
   $senha     = md5($senha);
+  $email     = mysqli_real_escape_string($conexao,$email);
   $query     = "SELECT * FROM usuarios WHERE email='{$email}' and senha='{$senha}'";
   $resultado = mysqli_query($conexao,$query);
   $usuario   = mysqli_fetch_assoc($resultado);
